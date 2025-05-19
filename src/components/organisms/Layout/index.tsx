8a8f4569a64c451e52b8f8115a3decb5bd412800
Samuel <samuel.montoya@udea.edu.co>
2025-05-19 13:32:37 -0500

    No hay aplicacion

--- src/components/organisms/Layout/index.tsx
+++ src/components/organisms/Layout/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 // import NavBar from "@/components/organisms/NavBar/";
-import NavMobile from "@/components/molecules/NavMobile";
+// import NavMobile from "@/components/molecules/NavMobile";
 
 import { ReactNode } from "react";
 
@@ -8,9 +8,7 @@ const Index = ({ children }: { children: ReactNode }) => {
   return (
     <div>
       <div className="hidden lg:block">{/* <NavBar /> */}</div>
-      <div className="block lg:hidden">
-        <NavMobile />
-      </div>
+      <div className="block lg:hidden">{/* <NavMobile /> */}</div>
       <div>{children}</div>
     </div>
   );
