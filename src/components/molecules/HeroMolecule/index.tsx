49fea137b12d906fb58ad394455fa3263b1eaac2
Samuel <samuel.montoya@udea.edu.co>
2025-05-19 13:08:44 -0500

    no entiendo x2

--- src/components/molecules/HeroMolecule/index.tsx
+++ src/components/molecules/HeroMolecule/index.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { HeroTitle, SubText } from "@/components/atoms/Titles";
-import Button from "@/components/atoms/Button";
+// import Button from "@/components/atoms/Button";
 
 const Index = () => {
   return (
     <div className="flex flex-col items-center gap-4 mt-8">
       <HeroTitle />
       <SubText text="Take your financial life online. Your Easybank account will be a one-stop-shop for spending, saving, budgeting, investing, and much more." />
-      <Button text="Request Invite." />
+      {/* <Button text="Request Invite." /> */}
     </div>
   );
 };
