72b3ecd6a268727740516f7a6a16ae1fd596426b
Samuel <samuel.montoya@udea.edu.co>
2025-05-19 13:05:06 -0500

    no entiendo

--- src/components/molecules/HeroMolecule/index.tsx
+++ src/components/molecules/HeroMolecule/index.tsx
@@ -7,7 +7,7 @@ const Index = () => {
     <div className="flex flex-col items-center gap-4 mt-8">
       <HeroTitle />
       <SubText text="Take your financial life online. Your Easybank account will be a one-stop-shop for spending, saving, budgeting, investing, and much more." />
-      <Button text="Request Invite" />
+      <Button text="Request Invite." />
     </div>
   );
 };
